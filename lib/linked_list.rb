d936e56a821dc96ba1e8548f14dbb6d5e5dba519
Andrew <andrewasmit@hotmail.com>
2023-05-11 11:20:42 -0500

    donept2

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,7 +9,7 @@ class LinkedList
 
   def reorder_linked_list
     return if head.nil?
-    
+
     odd = head
     even = head.next_node
 
@@ -20,9 +20,24 @@ class LinkedList
       odd = odd.next_node
 
       even.next_node = odd.next_node
+
       even = even.next_node
     end
     odd.next_node = even_head
   end
 
+
 end
+
+
+# Take 1-2-3-4-5 
+# turn it into 1-3-5-2-4
+
+# return is head doesn't have a next node
+# make odd and even variables
+# Keep track of the even head as head.next_node
+
+# ****FIRST ITERATION****
+# odd.next_node = even.next_node (makes the list look like  1 => 3)
+# odd (1) = odd.next_node(3)   ('odd' becomes 3)
+# even (2) = odd.next_node (4)
